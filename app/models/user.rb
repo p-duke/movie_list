30d20218108b2fd47ee76d1de96ca0f2b9e06ef0
p-duke <peteraaronduke@gmail.com>
2017-06-03 19:23:25 -0400

    Build out User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  has_many :movie_items
+
+  has_secure_password
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A\w+@\w+[.]\w+\z/, message: "must be a valid e-mail address."}
+  validates :password_digest, length: {minimum: 6}, format: {with: /[A-Z]/, message: "must have at least one capital letter."}
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:params) { { username: "pduke", email: "peter@gmail.com", password_digest: "$Password" } }
+  subject(:user) { User.new(params) }
+
+  describe "associations" do 
+    it { should have_many(:movie_items) }
+  end
+
+  describe "validations" do
+
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
+
+    context 'when the username is not present' do
+      before { user.username = nil }
+
+      it { should_not be_valid }
+
+      #should validate_presence_of(:username)
+    end
+
+    context 'when the email is not present' do
+      before { user.email = nil }
+
+      it { should_not be_valid }
+
+      #should validate_presence_of(:email)
+    end
+
+    context 'when the username is not unqiue' do
+      before { user.save }
+      let(:another_user) { User.new(params) }
+
+      it 'should not be valid' do
+        expect(another_user).to_not be_valid
+      end
+    end
+
+    context 'when the email is not unique' do
+      before { user.save }
+      let(:another_user) { described_class.new(params) }
+
+      it 'should not be valid' do
+        expect(another_user).to_not be_valid
+      end
+    end
+
+    context 'when the email is not formatted properly' do
+      before { user.email = 'pizza doughnut sandwich' }
+
+      it { should_not be_valid }
+
+      #should_not allow_value("spring-is-here!").for(:email)
+    end
+
+    context 'when the password has less than 6 characters' do
+      before { user.password_digest = "wee!" }
+
+      it { should_not be_valid }
+    end
+
+    context 'when the password doesnt have a capital letter' do
+      before { user.password_digest = "wee!" }
+
+      it { should_not be_valid }
+    end
+  end
 end
