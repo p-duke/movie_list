3a7f3636e0fedbbb235d71e2f232cf108ea8d3d9
p-duke <peteraaronduke@gmail.com>
2017-06-04 15:39:40 -0400

    Build out MovieItem model and test suite

--- app/models/movie_item.rb
+++ app/models/movie_item.rb
@@ -1,2 +1,5 @@
 class MovieItem < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, :plot, :user_id, :year, :director, :actors, :rated, :genre, presence: true
 end
--- spec/models/movie_item_spec.rb
+++ spec/models/movie_item_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe MovieItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:params) { { 
+    title: "Warrior Burrito",
+    plot: "A burrito that fights the forces of evil",
+    imdb_id: 12203,
+    user_id: 1,
+    year: 1987,
+    director: "Francis Ford CocaCola",
+    actors: 'Kevin Spacey, Bob Barker',
+    rated: 'PG',
+    genre: 'Action',
+    runtime: '1h 21min',
+    rotten_tomatoes: '78%'
+  } }
+
+  subject { described_class.new(params) }
+
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
+
+  describe "validations" do
+    it { should be_valid }
+
+    context 'when the user_id is not present' do
+      before { subject.user_id = nil }
+
+      it { should_not be_valid }
+    end
+
+    context 'when primary attributes are not present' do
+      before do
+        subject.title = nil 
+        subject.year = nil 
+        subject.director = nil
+        subject.rated = nil
+        subject.genre = nil
+      end
+
+      it { should_not be_valid }
+    end
+  end
 end
