8e992291040efcecf7bedd8049c93aa79cde6d1c
p-duke <peteraaronduke@gmail.com>
2017-06-06 17:43:20 -0400

    Build ApiQueries controller

--- app/controllers/api_queries_controller.rb
+++ app/controllers/api_queries_controller.rb
@@ -1,2 +1,15 @@
 class ApiQueriesController < ApplicationController
+
+  def index
+    @query = ApiQuery.new()
+    @query.search
+    @headers = @query.results["results"].first.keys
+  end
+
+  def create
+    @query = ApiQuery.new(params)
+    @query.search
+    render 'show'
+  end
+
 end
--- spec/controllers/api_queries_controller_spec.rb
+++ spec/controllers/api_queries_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ApiQueriesController, type: :controller do
+  describe "GET index" do
+    it 'renders the template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
 
+  describe "POST create" do
+    let(:params) { { title: "Waynes World" } }
+
+    it 'assigns @api_query' do
+      api_query = ApiQuery.new()
+      post :create, params
+      expect(response).to render_template('show')
+    end
+  end
 end
